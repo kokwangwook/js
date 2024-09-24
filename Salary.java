8ab270983a0281937bf0f3b7be8f7ff78415aabe
kokwangwook <multi618@gmail.com>
2024-09-24 21:45:24 +0900

    Create Salary.java

---
+++ Salary.java
@@ -0,0 +1,42 @@
+package com.example.salary;
+
+public class Salary {
+    private String grade;
+    private int hoBon;
+    private double salary2024;
+    private double salary2025;
+
+    // Getters and Setters
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public int getHoBon() {
+        return hoBon;
+    }
+
+    public void setHoBon(int hoBon) {
+        this.hoBon = hoBon;
+    }
+
+    public double getSalary2024() {
+        return salary2024;
+    }
+
+    public void setSalary2024(double salary2024) {
+        this.salary2024 = salary2024;
+    }
+
+    public double getSalary2025() {
+        return salary2025;
+    }
+
+    public void setSalary2025(double salary2025) {
+        this.salary2025 = salary2025;
+    }
+}
