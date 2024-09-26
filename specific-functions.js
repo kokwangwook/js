ffb972c71550a29289784557f0433bfbf51de648
kokwangwook <multi618@gmail.com>
2024-09-26 10:08:39 +0900

    Create specific-functions.js

---
+++ specific-functions.js
@@ -0,0 +1,9 @@
+// specific-functions.js (특정 페이지에서만 사용할 함수)
+
+function specificFunction1() {
+    console.log("Specific Function 1 from specific-functions.js");
+}
+
+function specificFunction2() {
+    console.log("Specific Function 2 from specific-functions.js");
+}
