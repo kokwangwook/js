1ce2805c264dc95c059363c975987b83b6538c62
kokwangwook <multi618@gmail.com>
2024-09-26 10:39:39 +0900

    Update specific-functions.js

--- specific-functions.js
+++ specific-functions.js
@@ -7,3 +7,11 @@ function specificFunction1() {
 function specificFunction2() {
     console.log("Specific Function 2 from specific-functions.js");
 }
+
+
+// specific-functions.js
+
+// 특정 페이지에서 호출되는 샘플 함수
+function showTestMessage() {
+    alert("This is a test message from specific-functions.js");
+}
