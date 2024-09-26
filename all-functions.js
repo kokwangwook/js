0b91ddb824ff7e32cac2507e84c265ac9307cced
kokwangwook <multi618@gmail.com>
2024-09-26 10:08:13 +0900

    Create all-functions.js

---
+++ all-functions.js
@@ -0,0 +1,11 @@
+// all-functions.js (10000개의 함수 중 일부 예시)
+
+function functionA() {
+    console.log("Function A from all-functions.js");
+}
+
+function functionB() {
+    console.log("Function B from all-functions.js");
+}
+
+// 이곳에 나머지 10000개의 함수들이 포함될 것입니다.
